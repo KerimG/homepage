ed94b5c1b55340b3e1852b91fc82ac23199bd246
Kerim Güney <kerim@gueney.org>
2017-06-21 18:29:36 +0200

    create basic page layout template

---
+++ resources/views/layout/page.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Kerim Güney's Homepage</title>
+    @yield('head')
+</head>
+<body>
+@yield('content')
+@yield('scripts')
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,5 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout.page')
 
-    <title>Kerim Güney's Homepage</title>
-
-</head>
-<body>
-</body>
-</html>
+@section('content')
+    <h1>Hello World!</h1>
+@stop
