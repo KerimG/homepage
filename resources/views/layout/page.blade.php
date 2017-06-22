ab231a3182509e391b734798e3feb79012e1df72
Kerim Güney <kerim@gueney.org>
2017-06-22 21:17:57 +0200

    version JavaScript and stylesheet compilations

--- resources/views/layout/page.blade.php
+++ resources/views/layout/page.blade.php
@@ -5,10 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Kerim Güney's Homepage</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     @yield('head')
 </head>
 <body>
 @yield('content')
 @yield('scripts')
+<script src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
