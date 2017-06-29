2df6c4218782e24f07ccd8ca958fb5cf21dfc24b
Kerim Güney <kerim@gueney.org>
2017-06-29 21:30:55 +0200

    move page template into layouts folder

--- resources/views/blog/frontpage.blade.php
+++ resources/views/blog/frontpage.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.page')
+@extends('layouts.page')
 
 @section('content')
     <h1>Hello</h1>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.page')
+@extends('layouts.page')
 
 @section('content')
     <div class="container">
