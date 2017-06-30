e8c3dd2fa2d65f22d067a637b88a70408d500de6
Kerim Güney <kerim@gueney.org>
2017-06-30 17:10:25 +0200

    get last 5 blog entries and display them in the frontpage view

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use App\BlogEntry;
 
 class BlogController extends Controller
 {
     public function frontpage()
     {
-        return view('blog.frontpage');
+        $latestBlogEntries = BlogEntry::orderBy('created_at', 'desc')->take(5)->get();
+        return view('blog.frontpage', ['latestBlogEntries' => $latestBlogEntries]);
     }
 }
--- resources/views/blog/frontpage.blade.php
+++ resources/views/blog/frontpage.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.page')
 
 @section('content')
-    <h1>Hello</h1>
+    <div class="container">
+        @foreach($latestBlogEntries as $blog_entry)
+            <article>
+                <h3>
+                    {{ $blog_entry->title }}
+                </h3>
+                <p class="slug">
+                    {{ $blog_entry->slug }}
+                </p>
+            </article>
+        @endforeach
+    </div>
 @stop
