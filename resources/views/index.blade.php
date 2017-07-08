cd5afc9ffd078ac4997874aaa01f589ae8c48a30
Kerim Güney <kerim@gueney.org>
2017-07-08 22:45:06 +0200

    removed those tacky separators on the homepage and adjusted some text/links

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <h1 class="page-title hidden">Kerim's Homepage</h1>
         <header>
             <div class="introduction">
-                <h2 class="introduction-title">
+                 <h2 class="introduction-title">
                     Hey, I'm Kerim. Nice to meet you!
                 </h2>
                 <p class="introduction-paragraph">
@@ -13,21 +13,23 @@
                 </p>
             </div>
         </header>
-        <div class="separator hidden--tablet-and-up">
-            <p>&#9830; &#9830; &#9830;</p>
-        </div>
         <section>
-            <h4 class="more-info-title">More about me and other interesting things:</h4>
+            <h4 class="more-info-title">How to contact me and other interesting things:</h4>
             <p>
-                If you would like to contact me, <a href="mailto:kerim@gueney.org">email me</a>.
+                You can email <a href="mailto:kerim@gueney.org">here</a>.
+            </p>
+            <p>
+                Befriend me on <a target="_blank" rel="noopener" href="https://www.xing.com/profile/Kerim_Gueney3">XING</a>.
             </p>
             <p>
                 Visit my <a target="_blank" rel="noopener" href="https://github.com/kerimg">GitHub</a> Profile.
             </p>
             <p>
-                Befriend me on <a target="_blank" rel="noopener" href="https://www.xing.com/profile/Kerim_Gueney3">XING</a>.
+                Check out the awesome <a target="_blank" rel="noopener" href="http://fai-project.org">FAI Project</a>
             </p>
-            <p>Check out the <a target="_blank" rel="noopener" href="http://fai-project.org">FAI Project</a></p>
+            <p>
+                or check out <a href="{{ route("blog") }}">my blog!</a>
+            </p>
         </section>
         <section>
             <h4 class="latest-entry-title">What I wrote about the other day:</h4>
